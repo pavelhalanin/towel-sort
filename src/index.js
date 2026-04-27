5866d19a5ce10e029d419fdd0c31c1ebae648907
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-04-27 23:08:03 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return [];
+  }
+
+  let resultArray = [];
+
+  matrix.forEach((element, index) => {
+    if (index % 2 === 0) {
+      resultArray = resultArray.concat(element);
+    } else {
+      resultArray = resultArray.concat(element.reverse());
+    }
+  });
+
+  return resultArray;
 };
